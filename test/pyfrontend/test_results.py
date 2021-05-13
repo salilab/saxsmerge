3c640a7347378852b7f9850ef7f89a5a2919c69b
Ben Webb <ben@salilab.org>
2021-05-12 17:49:14 -0700

    Port old Perl tests to Python

--- test/pyfrontend/test_results.py
+++ test/pyfrontend/test_results.py
@@ -7,6 +7,45 @@ saxsmerge = saliweb.test.import_mocked_frontend("saxsmerge", __file__,
                                                 '../../frontend')
 
 
+def get_summary_file():
+    return """Merge file
+  General
+   Filename: merged.dat
+   Number of points: 816
+   Data range: 0.01713 0.33480
+   447 points from profile 0 (180713_MBP-IVB_1mg.dat)
+   13 points from profile 1 (180713_MBP-IVB_2.5mg.dat)
+   356 points from profile 2 (180713_MBP-IVB_5mg.dat)
+  Gaussian Process parameters
+   mean function : Full
+   G : 23.548439 +- 0.610082
+   Rg : 25.133688 +- 0.976429
+   d : 1.313590 +- 11.304685
+   s : 0.360370 +- 79.795855
+   A : -4.938232 +- 0.217562
+   sigma2 : 21.339089 +- 0.151586
+   tau : 9.507107 +- -nan
+   lambda : 0.041534 +- 232.081966
+  Calculated Values
+   Q1 : 0.044627
+   Q1.Rg : 1.121638
+   I(0) : inf
+
+Input file 0
+   mean function : Simple
+   G : 13.989880 +- 1.351631
+   Rg : 45.703773 +- 0.861011
+   A : 1.857049 +- 1.503380
+   sigma2 : 18.321082 +- 0.344843
+   tau : 1.066421 +- 3.944389
+   lambda : 0.047426 +- 128.533419
+   Calculated Values
+    Q1 : 0.159873
+    Q1.Rg : 7.306783
+    I(0) : 107.007365
+"""
+
+
 class Tests(saliweb.test.TestCase):
     """Check results page"""
 
@@ -41,6 +80,43 @@ class Tests(saliweb.test.TestCase):
                 b'No output file was produced.*'
                 b'Please inspect the log file.*'
                 rb'saxsmerge\.log.*View SAXS Merge log file',
+                re.MULTILINE | re.DOTALL)
+            self.assertRegex(rv.data, r)
+
+    def test_ok_job(self):
+        """Test display of OK job"""
+        with saliweb.test.make_frontend_job('testjob4') as j:
+            j.make_file("summary.txt", contents=get_summary_file())
+            c = saxsmerge.app.test_client()
+            rv = c.get('/job/testjob4?passwd=%s' % j.passwd)
+            r = re.compile(
+                b'Output Files.*'
+                b'Summary file.*'
+                b'Merge Statistics.*'
+                b'mean function.*A.*G.*Rg.*sigma.*lambda.*'
+                rb'180713_MBP-IVB_1mg\.dat',
+                re.MULTILINE | re.DOTALL)
+            self.assertRegex(rv.data, r)
+
+    def test_ok_job_plots(self):
+        """Test display of OK job with plots"""
+        with saliweb.test.make_frontend_job('testjob5') as j:
+            j.make_file("summary.txt", contents=get_summary_file())
+            j.make_file('input.txt',
+                        "SubtrB1-A11b.dat=10\nSubtrB2-A11b.dat=10\n--auto\n")
+            for fname in ("mergeplots.js", "mergeinplots.js", "inputplots.js"):
+                j.make_file(fname, "")
+            c = saxsmerge.app.test_client()
+            rv = c.get('/job/testjob5?passwd=%s' % j.passwd)
+            r = re.compile(
+                b'Output Files.*'
+                b'Summary file.*'
+                b'Merge Statistics.*'
+                b'mean function.*A.*G.*Rg.*sigma.*lambda.*'
+                rb'180713_MBP-IVB_1mg\.dat.*'
+                rb'<h4>Merge Plots<\/h4>.*'
+                rb'<h4>Input Colored Merge Plots<\/h4>.*'
+                rb'<h4>Input Plots<\/h4>',
                 re.MULTILINE | re.DOTALL)
             self.assertRegex(rv.data, r)
 
--- test/pyfrontend/test_submit.py
+++ test/pyfrontend/test_submit.py
@@ -1,10 +1,21 @@
 import unittest
 import saliweb.test
 import os
+import re
 
 # Import the saxsmerge frontend with mocks
 saxsmerge = saliweb.test.import_mocked_frontend("saxsmerge", __file__,
                                                 '../../frontend')
+
+
+def get_default_submit_parameters():
+    return {'recordings': '3', 'gen_unit': 'Angstrom',
+            'gen_output': 'normal', 'gen_stop': 'merging',
+            'fit_param': 'Full', 'res_model': 'normal',
+            'class_alpha': '0.05', 'merge_param': 'Full',
+            'gen_npoints_val': '200', 'clean_cut': '0.1',
+            'res_ref': 'last', 'res_npoints': '200',
+            'merge_extrapol': '0'}
 
 
 class Tests(saliweb.test.TestCase):
@@ -22,6 +33,20 @@ class Tests(saliweb.test.TestCase):
             self.assertIn(
                 b'number of times each profile has been recorded must '
                 b'be at least 2!', rv.data)
+
+            data = get_default_submit_parameters()
+            profile = os.path.join(t, 'test.profile')
+            with open(profile, 'w'):
+                pass
+
+            # Successful submission (no email)
+            data['uploaded_file'] = open(profile, 'rb')
+            rv = c.post('/job', data=data)
+            self.assertEqual(rv.status_code, 200)
+            r = re.compile(b'Your job .* has been submitted.*'
+                           b'Results will be found',
+                           re.MULTILINE | re.DOTALL)
+            self.assertRegex(rv.data, r)
 
 
 if __name__ == '__main__':
